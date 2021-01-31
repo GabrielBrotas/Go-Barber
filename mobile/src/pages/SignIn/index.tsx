9be41b532f7f538262018b14a1f5bd2dedf8dd3b
GabrielBrotas <gabrielbrotas22@gmail.com>
2021-01-31 11:36:07 -0300

    styles

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,28 +1,59 @@
 import React from 'react';
-import { Image } from 'react-native';
-import { Container, Title } from './styles';
+import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import Input from '../../components/Input'
-import Button from '../../components/Button'
+import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
 const SignIn : React.FC =  () => {
+  const navigation = useNavigation();
+
   return (
-    <Container>
-      <Image source={logoImg} />
-
-      <Title>Faça seu Logon</Title>
-
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
-
-      <Button
-       onPress={() => console.log('a')}
+    <>
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === "ios" ? 'padding' : undefined}
+        enabled
       >
-        Entrar
-      </Button>
-    </Container>
+        <ScrollView
+          keyboardShouldPersistTaps="handled" // quando clicar fora ele fechar o teclado
+          contentContainerStyle={{ flex: 1}}
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            {/* Como os textos não tem animação de subir e descer quando o keyboardAvoidingView acionar vamos colocar o View para animar a view e o texto consequentemente vai junto */}
+            <View>
+              <Title>Faça seu Logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+            onPress={() => console.log('a')}
+            >
+              Entrar
+            </Button>
+
+            <ForgotPassword onPress={() => console.log('a')}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   )
 }
 
--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,7 +1,59 @@
 import React from 'react';
+import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles'
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
-const SignUp : React.FC =  () => <Container />
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+const SignUp : React.FC =  () => {
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === "ios" ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled" // quando clicar fora ele fechar o teclado
+          contentContainerStyle={{ flex: 1}}
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            {/* Como os textos não tem animação de subir e descer quando o keyboardAvoidingView acionar vamos colocar o View para animar a view e o texto consequentemente vai junto */}
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+            onPress={() => console.log('a')}
+            >
+              Entrar
+            </Button>
+
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={20} color="#ff9000" />
+        <BackToSignInText>Voltar para logon</BackToSignInText>
+      </BackToSignIn>
+    </>
+  )
+}
 
 export default SignUp;
