862d1c78f2308ed64fad6dd07a534bdc395b09c6
GabrielBrotas <gabrielbrotas22@gmail.com>
2021-01-31 10:32:59 -0300

    geral

---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { Container, Title } from './styles';
+
+import logoImg from '../../assets/logo.png';
+
+const SignIn : React.FC =  () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu Logon</Title>
+    </Container>
+  )
+}
+
+export default SignIn;
