0210307c37d6f8c012163442eb0d9b96925531a5
GabrielBrotas <gabrielbrotas22@gmail.com>
2021-03-07 16:27:22 -0300

    save users

--- server/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ server/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,6 +1,6 @@
 import { injectable, inject } from 'tsyringe';
 
-import ICahcheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -18,7 +18,7 @@ class ListProviderAppointmentsService {
     private appointmentsRepository: IAppointmentsRepository,
 
     @inject('CacheProvider')
-    private cacheProvider: ICahcheProvider,
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
--- server/src/modules/appointments/services/ListProviderService.ts
+++ server/src/modules/appointments/services/ListProviderService.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 
@@ -13,12 +14,27 @@ class ListProviderService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProvider({
-      except_user_id: user_id,
-    });
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
+
+    if (!users) {
+      users = await this.usersRepository.findAllProvider({
+        except_user_id: user_id,
+      });
+
+      console.log('A query no banco foi feita!');
+      // o cache vai ser diferente para cada usuario pois na lista de users não deve conter o proprio que esta fazendo a requisição
+      // o ':' no redis é uma subnivel de providers-list
+      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      // na primeira query do get providers vai ser executada em torno de 40ms, depois de salvo no redis o cache quando realizarmos a query de novo vai ser feita em torno de 5ms pois vai estar salvo em cache.
+    }
 
     return users;
   }
